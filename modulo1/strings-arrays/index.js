6343c77161a4617be6e52d786d005cdc6e4c4cf2
lumarin2802 <marinperezluciana@gmail.com>
2021-10-06 20:21:53 -0300

    exercicios-do-dia

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -1,3 +1,34 @@
+/Faça um programa que pergunte ao usuário
+// seu nome e seu e-mail. Em seguida, 
+//Imprima no console a seguinte mensagem:
+//O e-mail `emailDoUsuario` foi cadastrado com sucesso. 
+//Seja bem-vinda(o), `nomeDoUsuario`!
+
+/*const nome = prompt ("qual e o seu nome?")
+const email = prompt ("qual e seu email?")
+const fraseTemplateString = `O email ${email} foi cadastrado com sucesso Seja bem vinda(o) ${nome} `
+console.log(fraseTemplateString)*/
+
+/*const comidasPreferidas = [ "lasanha", "churrasco", "feijao", "risotto", "strogonoff" ]
+console.log(comidasPreferidas)
+const primeiroItem = comidasPreferidas[0]
+const segundoItem = comidasPreferidas[1]
+const terceiroItem = comidasPreferidas[2]
+const quartoItem = comidasPreferidas[3]
+const quintoItem = comidasPreferidas[4]
+
+console.log("Essas são as minhas comidas preferidas:  ")
+console.log(comidasPreferidas[0])
+console.log(comidasPreferidas[1])
+console.log(comidasPreferidas[2])
+console.log(comidasPreferidas[3])
+console.log(comidasPreferidas[4])*/
+
+
+
+
+
+
 //Exercicio1//
 
 /*const nome = prompt ("qual e o seu nome?")
@@ -41,4 +72,4 @@ console.log(novaListaDeTarefas)
 1. mensagens impresas a.undefined b.null c.11 d.3 e.(11) [3,19,5,6,7,8,9,10,11,12,..]
 Exercicio2 
 O valor impresso será "SUBI NUM ONIBUS EM MARROCOS"? 30
-vai imprimira frase em maiuscula e o numero 30 que refere a quantidade de items do arrey
+vai imprimir a frase em maiuscula e o numero 30 que refere a quantidade de items do arrey
