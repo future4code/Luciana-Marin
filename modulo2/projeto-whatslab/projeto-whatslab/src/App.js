2b63202d6161b368950a2e0ed6e41a5574536a78
lumarin2802 <marinperezluciana@gmail.com>
2022-03-30 00:11:31 -0300

    whatslab

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import QuadroDigitacao from './Componentes/ComponenteMensagens/QuadroDigitacao';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+`
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const QuadroChat = styled.div`
+  width: 400px;
+  border: 1px solid black;
+  border-radius: 1%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+
+
+class App extends React.Component {
+    
+  
+  render() {
+    return (
+      <MainContainer>
+        <QuadroChat>
+          <FooterContainer>
+            <QuadroDigitacao/>
+          </FooterContainer>
+        </QuadroChat>
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
